b728e4a0d9cc3c97cf49722181d5b92923c3d6e7
forellii <2008.mv.mv@gmail.com>
2026-08-01 12:52:59 -0300

    upt

--- script/index.js
+++ script/index.js
@@ -4,3 +4,23 @@ const nav = document.getElementsByTagName('nav')[0];
 button.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
+
+const joinButton = document.querySelector('.join');
+joinButton.addEventListener('click', () => {
+  const ip = 'redevenus.co';
+  const port = '10004';
+  const textToCopy = `${ip}:${port}`;
+
+  navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      const dialog = document.querySelector('.dialog');
+      dialog.classList.add('show');
+
+      setTimeout(() => {
+        dialog.classList.remove('show');
+      }, 4000);
+    })
+    .catch(err => {
+      console.error('Erro ao copiar para a área de transferência: ', err);
+    });
+});
